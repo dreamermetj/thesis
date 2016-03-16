034ab14f12cd294f85ae8a6c43596bd536717ffe
tianjun <tianjun@fenbi.com>
2016-03-16 18:50:17 +0800

    ADD:20160316

--- source/code/NLPIR/nlpir.py
+++ source/code/NLPIR/nlpir.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 from ctypes import *
 
-libFile = '/Users/Dreamerlhp/workspace/thesis/source/code/NLPIR/nlpir/libNLPIR.so'
+libFile = '/Users/Dreamer/workspace/thesis/source/code/NLPIR/nlpir/libNLPIR.so'
 
 dll =  CDLL(libFile)
 def loadFun(exportName, restype, argtypes):
@@ -15,16 +15,11 @@ Init = loadFun('NLPIR_Init',c_int, [c_char_p, c_int, c_char_p])
 Exit = loadFun('NLPIR_Exit',c_bool, None)
 ParagraphProcess = loadFun('NLPIR_ParagraphProcess',c_char_p, [c_char_p, c_int])
 
-if not Init('/Users/Dreamerlhp/workspace/thesis/source/code/NLPIR',1,''): # 1 for utf-8 code
+if not Init('/Users/Dreamer/workspace/thesis/source/code/NLPIR',1,''): # 1 for utf-8 code
     print("Initialization failed!")
     exit(-111111)
 
-
-
-
-
-
-
-
-
-
+p = '天涯海角，海角天涯。'
+t = ParagraphProcess(p,c_int(1))
+print t
+Exit()
