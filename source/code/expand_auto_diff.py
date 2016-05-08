2c864ea880f5f0474cde162ba1091de812fb7331
Dreamer <61347084@qq.com>
2016-05-09 00:39:22 +0800

    ADD: 20160509

--- source/code/expand_auto_diff.py
+++ source/code/expand_auto_diff.py
@@ -52,7 +52,7 @@ def writef(f,_list):
 
 from nlpir import *
 VARSIGN = '…'.decode('utf-8')
-out = open('../extracted/expand/auto_diff_10','w')
+out = open('../extracted/expand/auto_diff','w')
 for i in diff_pair:
   print 'start processing ' + str(i) + ' ...'
   f=open('../lexDic/1234/c'+i[0]+'.pure')
@@ -63,57 +63,89 @@ for i in diff_pair:
   f.close()
   out.write('==== ' + i[0]+' : '+i[1]+ ' ===='+'\n')
   inter=less&more
+  #全变换表trd
+  trd = dict()
+  #临时表tmpd，记录交集的词，便于判断词是否出现在交集中
+  tmpd = dict()
+  for w in inter:
+    tmpd[w]=True
+    sw=''.join(sorted(w))
+    if sw not in trd:
+      trd[sw] = [w]
+    else:
+      trd[sw].append(w)
   diff=more-less
   #结果表rsd，记录diff中每个词是否在inter中有相应的extends形式
   rsd = []
   for w in diff:
-    protos = []
-    unislot = []
-    isUniSlot = False
-    if w in rev:
-      for c in rev[w]:
-        proto = [c,[]]
-        mode = ''
-        for idx in range(4):
-          if c[idx] != VARSIGN:
-            mode += str(idx+1)
-        for ins in d[mode][c]:
-          if ins == w:
-            continue
-          if ins in inter:
-            proto[1].append(ins)
-        if len(proto[1]) >= 10:
-          protos.append(proto)
-          if len(mode) == 3:
-            isUniSlot = True
-            unislot.append(proto)
-    if isUniSlot: #单槽次之，等级为1
-      rsd.append([w,1,unislot])
-    elif protos:
-      alterslot = []
-      duplicate = []
-      for proto in protos:
-        if (proto[0][0] == VARSIGN and proto[0][2] == VARSIGN) or (proto[0][1] == VARSIGN and proto[0][3] == VARSIGN):
-          alterslot.append(proto)
-          if proto[0][:2] == proto[0][2:]:
-            duplicate.append(proto)
-      if duplicate: #交替复现再次，等级为2
-        rsd.append([w,2,duplicate])
-      elif alterslot: #交替双槽再次，等级为3
-        rsd.append([w,3,alterslot])
-      else: #一般双槽等级为4
-        rsd.append([w,4,protos])
+    #print '    processing ' + w + ' ...'
+    sw = ''.join(sorted(w))
+    if sw in trd: #全变换等级最高，为0
+      rsd.append([w,0,trd[sw]])
+    else:
+      protos = []
+      unislot = []
+      isUniSlot = False
+      if w in rev:
+        for c in rev[w]:
+          proto = [c,[]]
+          mode = ''
+          for idx in range(4):
+            if c[idx] != VARSIGN:
+              mode += str(idx+1)
+          for ins in d[mode][c]:
+            if ins == w:
+              continue
+            if ins in inter:
+              proto[1].append(ins)
+          if proto[1]:
+            protos.append(proto)
+            if len(mode) == 3:
+              isUniSlot = True
+              unislot.append(proto)
+      if isUniSlot: #单槽次之，等级为1
+        rsd.append([w,1,unislot])
+      elif w[0] == w[2] or w[1] == w[3]: #叠字对举等级为2
+        rsd.append([w,2])
+      elif protos:
+        alterslot = []
+        for proto in protos:
+          if (proto[0][0] == VARSIGN and proto[0][2] == VARSIGN) or (proto[0][1] == VARSIGN and proto[0][3] == VARSIGN):
+            alterslot.append(proto)
+        if alterslot: #格式对举等级为3
+          rsd.append([w,3,alterslot])
+      else:
+        temp = ParagraphProcess(w.encode('utf-8'),c_int(1)).strip().split(' ')
+        if len(temp) == 2:
+          rsd.append([w,7])
+          continue
+        temp = re.split(r'[ ]+',ParagraphProcess(' '.join(w).encode('utf-8'),c_int(1)).strip())
+        tags = map(lambda x:x.split('/')[1][0],temp)
+        if tags[0] == tags[2] and tags[1] == tags[3]:
+          rsd.append([w,4]) #词性对举等级为4
+        elif tags[1] == tags[3]:
+          biw = w[0]+w[2]
+          if len(ParagraphProcess(biw.encode('utf-8'),c_int(1)).split('/')) == 2:
+            rsd.append([w,5]) #扩展对举等级为5
+        elif tags[0] == tags[2]:
+          biw = w[1]+w[3]
+          if len(ParagraphProcess(biw.encode('utf-8'),c_int(1)).split('/')) == 2:
+            rsd.append([w,5]) #扩展对举等级为5
+        else:
+          rsd.append([w,6]) #啥也没有
   s = sorted(rsd,key=lambda x:x[1])
-  count = [0,0,0,0,0,0,0]
+  count = [0,0,0,0,0,0,0,0]
   for si in s:
+    '''
     out.write(si[0].encode('utf-8') + '\t' + str(si[1]))
     if len(si) > 2:
       out.write('\t')
       writef(out,si[2])
     out.write('\n')
+    '''
     count[si[1]] += 1
   out.write('********\n')
-  for ci in range(7):
+  for ci in range(8):
     out.write(str(ci)+': '+str(count[ci])+'\n')
   out.write('********\n')
 out.close()
